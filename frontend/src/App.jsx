20f70167a44fcebf148d11f550ec4d1f368a7d6f
Arpit <arpitjoshi213.com>
2025-04-22 16:51:24 +0530

    merge branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Signup from './pages/Signup';
-import Login from './pages/Login';  // Import the Login page
+import Login from './pages/Login';  
 import Start from './pages/start';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Define your routes here */}
+    
         <Route path="/" element={<Start />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
